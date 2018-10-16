d170f07bb86796aadb58e652d9c709fdb9560105
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2018-10-16 12:47:15 +0300

    Add setuptools installation to script

--- install_python_deps.sh
+++ install_python_deps.sh
@@ -7,6 +7,7 @@ pythondir=$currentdir/site-packages
 mkdir -p $pythondir
 export PYTHONPATH=$PYTHONPATH:$pythondir
 
+# pycparser
 pushd $currentdir
 git clone https://github.com/eliben/pycparser.git
 
@@ -17,6 +18,18 @@ popd
 rm -rf ./pycparser
 popd
 
+pushd $current_dir
+curl -O https://files.pythonhosted.org/packages/6e/9c/6a003320b00ef237f94aa74e4ad66c57a7618f6c79d67527136e2544b728/setuptools-40.4.3.zip
+unzip setuptools-40.4.3.zip
+pushd setuptools-40.4.3
+python setup.py install --install-lib $pythondir --install-scripts=$pythondir/bin
+popd
+
+rm setuptools-40.4.3.zip
+rm -rf ./setuptools-40.4.3
+popd
+
+# Mako
 pushd $currentdir
 makover=1.0.7
 curl -O https://files.pythonhosted.org/packages/eb/f3/67579bb486517c0d49547f9697e36582cd19dafb5df9e687ed8e22de57fa/Mako-$makover.tar.gz

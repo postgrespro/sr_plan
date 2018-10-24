118cf964041e22109393355312946b63c5c63718
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2018-10-24 17:59:05 +0300

    Remove unneeded files

--- nodes.h
+++
@@ -1,8 +0,0 @@
-#define __attribute__(x)
-#include "pg_config.h"
-typedef int __int128
-
-#include "postgres.h"
-#include "nodes/plannodes.h"
-#include "nodes/parsenodes.h"
-#include "nodes/value.h"
